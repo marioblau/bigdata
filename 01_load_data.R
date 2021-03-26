e5f02bd0c70bcaf0f9558d4475bff8c2e69b0910
sddiener <55442757+sddiener@users.noreply.github.com>
2021-03-26 15:15:48 +0100

    Added project disposition

---
+++ 01_load_data.R
@@ -0,0 +1,28 @@
+#-------------------------------------------------------------------------------------
+# 01) Load Data
+#-------------------------------------------------------------------------------------
+# Packages and settings
+rm(list = ls())
+library(tidyverse)
+library(pryr)
+library(data.table)
+
+DATA_PATH <- "../bigdata/data/unbalaced_20_80_dataset.csv"
+
+# sample data for debugging
+SAMPLE <- TRUE
+SAMPLE_FRAC <- 0.01
+
+
+# DATA IMPORT ----------------
+mem_change(df <- fread(DATA_PATH))
+
+if(SAMPLE== TRUE){
+  mem_change(df_smpl <- df %>% sample_frac(SAMPLE_FRAC))
+  mem_change(rm(df))
+}
+
+gc()
+
+df %>% colnames
+summary(df)
