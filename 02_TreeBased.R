18eae680b0e8797366bd626efa861c31b06f2b6f
sddiener <55442757+sddiener@users.noreply.github.com>
2021-05-01 11:12:44 +0200

    minor changes

--- 02_TreeBased.R
+++ 02_TreeBased.R
@@ -7,25 +7,17 @@ library(tidyverse)
 library(pryr)
 library(data.table)
 
-DATA_PATH <- "../bigdata/data/unbalaced_20_80_dataset.csv"
+# LOAD DATA ----------------
+DATA_PATH <- "../bigdata/data/final_dataset_preprocessed.csv"
 
 # sample data for debugging
 SAMPLE <- TRUE
 SAMPLE_FRAC <- 0.01
 
-
-# LOAD DATA ----------------
+# DATA IMPORT ----------------
 mem_change(df <- fread(DATA_PATH))
 
-if(SAMPLE== TRUE){
-  mem_change(df <- df %>% sample_frac(SAMPLE_FRAC))
-  # mem_change(rm(df))
-}
 
-gc()
 
-df %>% colnames
-summary(df$Label)
-class(df)
 
-df$Label %>% head(6)
+
