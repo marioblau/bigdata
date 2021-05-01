d5ebf0c083a9da111a055235609f9a9ae42b50ae
sddiener <55442757+sddiener@users.noreply.github.com>
2021-05-01 10:47:03 +0200

    converted binary character label to numeric

--- 01_load_data.R
+++ 01_load_data.R
@@ -7,7 +7,8 @@ library(tidyverse)
 library(pryr)
 library(data.table)
 
-DATA_PATH <- "../bigdata/data/unbalaced_20_80_dataset.csv"
+DATA_PATH <- "../bigdata/data/final_dataset.csv"  # original balanced
+SAVE_PATH <- "../bigdata/data/final_dataset_preprocessed.csv"  #
 
 # sample data for debugging
 SAMPLE <- TRUE
@@ -19,17 +20,19 @@ mem_change(df <- fread(DATA_PATH))
 
 if(SAMPLE== TRUE){
   mem_change(df_smpl <- df %>% sample_frac(SAMPLE_FRAC))
-  mem_change(rm(df))
 }
 
-gc()
-
 df %>% colnames
-summary(df)
+# summary(df)
 
 # PREPROCESSING ----------------
 
-# convert Label to numeric
+# convert Label to numeric  (ddos = 1, benign = 0)
 df <- df %>%
   mutate(Label01 = as.numeric(as.factor(Label))-1)  # -1 to convert labels from (1,2) to (0,1)
 
+sum(df$Label01) / length(df$Label01) # ~ 50/50 ddos vs benign
+summary(df$Label01)
+
+# SAVE ------------
+fwrite(df, SAVE_PATH)
