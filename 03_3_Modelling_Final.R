e8eb4d0659d561a76dd78ea016cd982cc23ffa9e
stefan <sdiener7@gmail.com>
2021-05-23 14:13:10 +0200

    Ran Naive Bayes on 100% data (seed 2021)

--- 03_3_Modelling_Final.R
+++ 03_3_Modelling_Final.R
@@ -19,7 +19,9 @@ suppressPackageStartupMessages({
 
   # Packages ML models (faster implementation)
   library(ranger)
+  library(biglasso)
   library(FNN)
+  library(yaImpute)
   library(LiblineaR)
 
   # Package parallel computing
@@ -33,25 +35,28 @@ suppressPackageStartupMessages({
 })
 
 SMPL_FRAC <- 1
-
 DATA_PATH <- paste0("data/final_dataset_preprocessed_sample100_scaled.csv")
+
+load("results/scaled/train_test_sample100_seed2021.RData")
 
 print(paste0("Start FINAL Modelling  on ", SMPL_FRAC*100, "% of data ", Sys.time()))
 
 #p <- profvis({ # sample rate = 10ms
 # LOAD DATA ----------------
 print(paste("Loading data from: ",DATA_PATH))
-df <- fread(DATA_PATH) %>% #TODO remove df again? chck with memchange
-  sample_frac(.,SMPL_FRAC) # sample only if SAMPLE variable is true
+set.seed(2021)
+df <- fread(DATA_PATH) %>%
+  sample_frac(.,SMPL_FRAC, )
 
 # TRAIN TEST SPLIT ----------------
 print(paste("Create train-test split...", Sys.time()))
 df$id <- 1:nrow(df)
+set.seed(2021)
 train <- df %>% sample_frac(.95)
 test  <- anti_join(df, train, by = 'id')
 rm(df)
 print(paste("Save train-test split...", Sys.time()))
-#save(train, test, file = paste0("results/scaled/train_test_sample", SMPL_FRAC*100,".RData"))
+save(train, test, file = paste0("results/scaled/train_test_sample", SMPL_FRAC*100,".RData"))
 
 
 # DECISION TREE --------------
@@ -82,8 +87,8 @@ print(paste("Training XG-Boost", Sys.time()))
 
 train_mat <- as.matrix(train)
 test_mat <- as.matrix(test)
-train_dmat <- xgb.DMatrix(data = train_mat[,-c(1)], label = train_mat[,1])
-test_dmat <- xgb.DMatrix(data = test_mat[,-c(1)], label = test_mat[,1])
+train_dmat <- xgb.DMatrix(data = train_mat[,-1], label = train_mat[, 1])
+test_dmat <- xgb.DMatrix(data = test_mat[,-1], label = test_mat[, 1])
 
 cl <- makePSOCKcluster(4)
 registerDoParallel(cl, cores = detectCores(all.tests = FALSE, logical = TRUE))
@@ -94,20 +99,22 @@ stopCluster(cl)
 
 print(paste("Save XG-Boost", Sys.time()))
 save(xgb, file = paste0("results/scaled/xgboost_sample", SMPL_FRAC*100,".RData"))
-rm(xgb, train_mat, test_mat, train_dmat, test_dmat)
+rm(xgb)
 
 # LOGISTIC REGRESSION --------
 print(paste("Training Logistic Regression", Sys.time()))
+xtrain_bigmat <- as.big.matrix(train[,-1])
+ytrain_bigmat <- as.big.matrix(train$Label)
 
-glm <- LiblineaR(train[,-1], train$Label, type = 1) # type:   0 – L2-regularized logistic regression (primal)
+glm <- biglasso(xtrain_bigmat, ytrain_bigmat, family = "binomial", ncores = detectCores(all.tests = FALSE, logical = TRUE))
 
 print(paste("Save Logistic Regression", Sys.time()))
 save(glm, file = paste0("results/scaled/logregression_sample", SMPL_FRAC*100,".RData"))
 rm(glm)
 
-# KNN --------
+# KNN -------- #TODO try ANN from yaImpute package
 print(paste("Training KNN", Sys.time()))
-knn <- FNN::knn(train[,-c(1)],test[,-c(1)],cl=train$Label,k=3,prob=TRUE)
+knn <- FNN::knn(train[,-1], test[, -1], cl=train$Label, k=3, prob=TRUE)
 
 print(paste("Save KNN", Sys.time()))
 save(knn, file = paste0("results/scaled/knn_sample", SMPL_FRAC*100,".RData"))
@@ -118,7 +125,7 @@ print(paste("Training Naive Bayes", Sys.time()))
 
 cl <- makePSOCKcluster(4)
 registerDoParallel(cl, cores = detectCores(all.tests = FALSE, logical = TRUE))
-nb <- naiveBayes(x = train[,-c(1)], y = train[,c(1)])
+nb <- naiveBayes(x = train[,-1], y = train[, 1])
 stopCluster(cl)
 
 print(paste("Save Naive Bayes", Sys.time()))
